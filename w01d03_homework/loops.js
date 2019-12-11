a810fa6c0e2579732fa37b7b454d4c145e69e7de
Nancy Schrader <katelanschrader7@gmail.com>
2019-12-10 23:35:49 -0600

    finishedloop practice

---
+++ w01d03_homework/loops.js
@@ -0,0 +1,20 @@
+// 1. Write a for loop that logs the numbers 0 through 100
+for (let i = 0; i < 100; i++) {
+    console.log(i);
+  }
+// 2. loop that logs numbers desending from 99
+for (let i = 100; i > -1; i--) {
+    console.log(i);
+  }
+// 3. loop for evens
+for ( i=0 ; i<100; i+=2 ) {
+    console.log(i); 
+}
+// 6. loop for odd counting down
+for (let i = 101; i > -1; i-=2) {
+    console.log(i);
+}
+// 9.Create a loop that logs the numbers from 3-90 that are multiples of 3 (ascending)
+for (let i = 3; i < 91; i+=3) {
+    console.log(i);
+}

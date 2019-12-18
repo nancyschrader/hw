f39d69a69896dcf284e0e95a3b1788a5d52e2a3d
Nancy Schrader <katelanschrader7@gmail.com>
2019-12-18 16:39:48 -0600

    Everything completed for this part

--- w02d03_homework/solutions.js
+++ w02d03_homework/solutions.js
@@ -18,3 +18,12 @@ const checkPalindrome = (string) => {
 console.log(checkPalindrome("Radar"));
 console.log(checkPalindrome("Borscht"));
 
+// Sum Array
+const sumArray = (arr) => {
+    let sum = 0;
+    for (let i = 0; i < arr.length; i++){
+        sum += arr(i)
+    }
+}
+console.log(sumArray([1, 2, 3, 4, 5, 6]));
+

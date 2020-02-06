95b5c26596a9f161f5a58ff362a3d73ca6458e17
Nancy Schrader <katelanschrader7@gmail.com>
2020-02-06 11:11:44 -0600

    cCommit 1 - I added my first element and it was fun

--- w05d03_homework/app.js
+++ w05d03_homework/app.js
@@ -1,5 +1,14 @@
-if (typeof jQuery == "undefined") {
-  console.log("oops! I still have to link my jQuery properly!");
-} else {
-  console.log("I did it! I linked jQuery and this js file!");
-}
+//if (typeof jQuery == "undefined") {
+  //console.log("oops! I still have to link my jQuery properly!");
+//} else {
+  //console.log("I did it! I linked jQuery and this js file!");
+//}
+
+
+$(() => {
+    const $div = $("container");
+    //console.log($div);
+    const $h1 = $("<h1>").text( "The Magical World of Hogwarts" );
+    //console.log($h1);
+    $div.append($h1);
+});

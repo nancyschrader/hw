8d13d481ed12a77856a2f3d27525fcf11c70fe08
Nancy Schrader <katelanschrader7@gmail.com>
2020-02-10 16:05:41 -0600

    Greetings express application created

--- w05d03_homework/app.js
+++ w05d03_homework/app.js
@@ -6,9 +6,15 @@
 
 
 $(() => {
-    const $div = $("container");
+    var $div = $("container");
     //console.log($div);
-    const $h1 = $("<h1>").text( "The Magical World of Hogwarts" );
+    var $h1 = $("<h1>").text( "The Magical World of Hogwarts" );
+    $("container").append('<h1>The Magical World of Hogwarts')
     //console.log($h1);
     $div.append($h1);
+    var $h2 = $("<h2>").text("Nancy Schrader").append;
+    var $h3 = $("<h3>").text("Ravenclaw").append;
+    $("h4").addClass("pet").append;
+
+    console.log($h3);
 });
---
+++ w09d02_homework/server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+
+const app = express();
+
+ app.get("/greeting/:name", (req, res)=>{
+    res.send(`Hello, ${req.params.name}`)
+ });
+
+
+
+app.listen(3000), () => {
+    console.log("Hello, Welcome.")
+};

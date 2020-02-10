3b3b653072127bf0a1575a436b93589fa0a9dedc
Nancy Schrader <katelanschrader7@gmail.com>
2020-02-10 16:23:20 -0600

    GTip calculator created.

--- w09d02_homework/server.js
+++ w09d02_homework/server.js
@@ -6,7 +6,9 @@ const app = express();
     res.send(`Hello, ${req.params.name}`)
  });
 
-
+app.get("/tip/:total/:tip", (req, res)=> {
+    res.send(`Your tip is: $${req.params.total*req.params.tip/100}`)
+});
 
 app.listen(3000), () => {
     console.log("Hello, Welcome.")

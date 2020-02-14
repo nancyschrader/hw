95fb098b18cc822359d09ffe27cc9387d64715ef
Nancy Schrader <katelanschrader7@gmail.com>
2020-02-14 16:22:11 -0600

    Connectws mt database, can see json in the browser

--- pokemon_app/server.js
+++ pokemon_app/server.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const app = express();
 const port = 3001;
+const pokemon = require("./models/pokemon.js");
 
 app.get("/", (req, res)=> {
     res.send('Welcome to the Pokemon App!')
 });
+app.get("/pokemon", (req, res) => {
+    res.send(pokemon)
+})
 
 app.listen(port, () => {
     console.log("listening on port", port)

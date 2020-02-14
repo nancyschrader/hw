4c10119c991dc40a085914765a8bef054ab2c6b6
Nancy Schrader <katelanschrader7@gmail.com>
2020-02-14 16:38:07 -0600

    Cindex.ejs view rendered at pokemon route

--- pokemon_app/server.js
+++ pokemon_app/server.js
@@ -7,7 +7,7 @@ app.get("/", (req, res)=> {
     res.send('Welcome to the Pokemon App!')
 });
 app.get("/pokemon", (req, res) => {
-    res.send(pokemon)
+    res.render("index.ejs", {pokemon: pokemon[req.params]});
 })
 
 app.listen(port, () => {

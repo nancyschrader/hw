dcb4bf49a48b4146aecb0798783466730d150004
Nancy Schrader <katelanschrader7@gmail.com>
2020-02-22 11:31:45 -0600

    Added basic folders and downloaded everything needed

--- Budgtr/server.js
+++ Budgtr/server.js
@@ -5,10 +5,18 @@ const port = 3000;
 
 app.use(express.urlencoded());
 app.use(methodOverride("_method"));
+app.use(express.static("public"));
 
+const Budget = require("/models/budget.js");
 
-
-
+//INDEX
+app.get("/bugets", (req,res) => {
+    res.render('index.ejs, {ledger: Budget}')
+});
+//SHOW
+app.get("/bugets/:index", (req,res) => {
+    res.render("show.ejs", {ledger: Budget[req.params.index]})
+});
 
 
 
